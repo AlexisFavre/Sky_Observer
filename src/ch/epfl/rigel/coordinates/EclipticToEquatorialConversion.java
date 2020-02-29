bab61ddd9c022b58662fffcbcaeb06a963c9ecdf
augst1 <augustin.allardaugustin@epfl.ch>
2020-02-29 10:41:32 +0100

    Begin of test epsilon and eqToHor

--- src/ch/epfl/rigel/coordinates/EclipticToEquatorialConversion.java
+++ src/ch/epfl/rigel/coordinates/EclipticToEquatorialConversion.java
@@ -5,24 +5,34 @@ import ch.epfl.rigel.astronomy.Epoch;
 import java.time.ZonedDateTime;
 import java.util.function.Function;
 
-public final class EclipticToEquatorialConversion implements Function<EclipticCoordinates, EquatorialCoordinates> {
+public final class EclipticToEquatorialConversion implements Function<EquatorialCoordinates, HorizontalCoordinates> {
 
     private double sinOfEpsilon;
-    private double cosOfEpsilon;
-    public EclipticToEquatorialConversion(ZonedDateTime when) {
-        double T = Epoch.J2000.julianCenturiesUntil(when);
-        double epsilon = 0.00181 * T*T*T - 0.0006 * T*T - 46.815 * T + 0;// TODO Replace 0 by right formula
+    private double cosOfEpsilon; // TODO Be sure about the value that should be pre-computed
+    public EclipticToEquatorialConversion(ZonedDateTime when) { // TODO Immutable?
+        double epsilon = epsilon(when);
         sinOfEpsilon = Math.sin(epsilon);
         cosOfEpsilon = Math.cos(epsilon);
     };
 
 
+    // TODO Hashcode & equals not found
+
+    public static double epsilon(ZonedDateTime when) {
+        double T = Epoch.J2000.julianCenturiesUntil(when);
+        return 0.00181*3600 * T*T*T - 0.0006*3600 * T*T - 46.815*3600 * T + 23 + (26 + 21.45/60)/60;
+    }
+
     @Override
-    public EquatorialCoordinates apply(EclipticCoordinates eclipticCoordinates) {
-        double lambda = eclipticCoordinates.lon();
-        double beta = eclipticCoordinates.lat();
+    public HorizontalCoordinates apply(EquatorialCoordinates equatorialCoordinates) {
+        double lambda = equatorialCoordinates.lon();
+        double beta = equatorialCoordinates.lat();
         double alpha = Math.atan((Math.sin(lambda) * cosOfEpsilon - Math.tan(beta) * sinOfEpsilon) / Math.cos(lambda));
         double gamma = Math.asin(Math.sin(beta) * cosOfEpsilon + Math.cos(beta) * sinOfEpsilon * Math.sin(lambda));
-        return EquatorialCoordinates.of(alpha, gamma);
+        return HorizontalCoordinates.of(alpha, gamma);
     }
+
+    /*private double dmsToDegrees(double degrees, double minutes, double seconds) {
+        return (Math.abs(minutes) + Math.abs(seconds/60))/60 + Math.abs(degrees);
+    }*/
 }
---
+++ src/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversion.java
@@ -0,0 +1,30 @@
+package ch.epfl.rigel.coordinates;
+
+import ch.epfl.rigel.astronomy.Epoch;
+
+import java.time.ZonedDateTime;
+import java.util.function.Function;
+
+public final class EquatorialToHorizontalConversion implements Function<EclipticCoordinates, EquatorialCoordinates> {
+
+    private double sinOfEpsilon;
+    private double cosOfEpsilon;
+    public EquatorialToHorizontalConversion(ZonedDateTime when, GeographicCoordinates where) { // TODO Immuable?
+        double T = Epoch.J2000.julianCenturiesUntil(when);
+        double epsilon = 0.00181 * T*T*T - 0.0006 * T*T - 46.815 * T + 0;// TODO Replace 0 by right formula
+        sinOfEpsilon = Math.sin(epsilon);
+        cosOfEpsilon = Math.cos(epsilon);
+    };
+
+
+    // TODO Hascode & equals not found
+
+    @Override
+    public EquatorialCoordinates apply(EclipticCoordinates eclipticCoordinates) {
+        double lambda = eclipticCoordinates.lon();
+        double beta = eclipticCoordinates.lat();
+        double alpha = Math.atan((Math.sin(lambda) * cosOfEpsilon - Math.tan(beta) * sinOfEpsilon) / Math.cos(lambda));
+        double gamma = Math.asin(Math.sin(beta) * cosOfEpsilon + Math.cos(beta) * sinOfEpsilon * Math.sin(lambda));
+        return EquatorialCoordinates.of(alpha, gamma);
+    }
+}
