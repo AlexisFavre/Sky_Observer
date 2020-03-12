22487d61810aa600fbc476546cd9df2e3a9b2ee0
augst1 <augustin.allardaugustin@epfl.ch>
2020-03-12 20:15:28 +0100

    All tests done CCenterForParallel not passing

--- test/ch/epfl/rigel/coordinates/MyCartesianCoordinatesTest.java
+++ test/ch/epfl/rigel/coordinates/MyCartesianCoordinatesTest.java
@@ -6,11 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MyCartesianCoordinatesTest {
 
+    final CartesianCoordinates TRIVIAL = CartesianCoordinates.of(1.2, 3.7);
+
     @Test
     void of() {
+        assertEquals(1.2, TRIVIAL.x(), 1.0e-10);
+        assertEquals(3.7, TRIVIAL.y(), 1.0e-10);
     }
 
     @Test
     void testToString() {
+        assertEquals("(x=1.2000, y=3.7000)", TRIVIAL.toString());
     }
 }
